6c937cfd97495aaffc24c8921c4fbe2d861b125b
Neil Jeffery <nwjeffery@gmail.com>
2025-12-03 17:42:32 +0000

    Day 2 Part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,7 @@
 from day2_input import input_data as input_data
 
+import textwrap
+
 
 def is_valid(value: str) -> bool:
     """Returns False if first and second half of string are equal"""
@@ -10,26 +12,37 @@ def is_valid(value: str) -> bool:
 
 def is_valid_2(value: str) -> bool:
     """Returns False if string is made up of repeating substrings"""
-    if len(value) % 2:
-        return True
-    return value[: len(value) // 2] != value[len(value) // 2 :]
+    for i in range(1, len(value) // 2 + 1):
+        if len(value) % i:
+            continue
+        check = set(textwrap.wrap(value, i))
+        if len(check) == 1:
+            return False
+    return True
 
 
-def get_invalid_ids(start: int, end: int) -> list[int]:
+def get_invalid_ids(start: int, end: int, check_function: callable) -> list[int]:
     """Return list of invalid IDs"""
     invalid_ids = []
     for value in range(start, end + 1):
-        if is_valid(str(value)):
+        if check_function(str(value)):
             continue
         invalid_ids.append(value)
+    print(invalid_ids)
     return invalid_ids
 
 
 def main():
     items = list(map(lambda x: x.split("-"), input_data.split(",")))
+
     total = 0
     for start, end in items:
-        total += sum(get_invalid_ids(int(start), int(end)))
+        total += sum(get_invalid_ids(int(start), int(end), is_valid))
+    print(total)
+
+    total = 0
+    for start, end in items:
+        total += sum(get_invalid_ids(int(start), int(end), is_valid_2))
     print(total)
 
 
