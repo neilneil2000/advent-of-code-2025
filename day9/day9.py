54ea2ec1efa9be6a0bcd0ac1db740f29113664fa
Neil Jeffery <nwjeffery@gmail.com>
2025-12-09 10:48:44 +0000

    Day 9 Part 1

---
+++ day9/day9.py
@@ -0,0 +1,25 @@
+from day9_input import input_data as input_data
+
+
+def get_area(a, b):
+    x = abs(a[0] - b[0]) + 1
+    y = abs(a[1] - b[1]) + 1
+    return x * y
+
+
+def main():
+    corners = list(
+        map(
+            lambda x: (int(x[0]), int(x[1])),
+            map(lambda x: x.split(","), input_data.splitlines()),
+        )
+    )
+    best = 0
+    for i, a in enumerate(corners):
+        for b in corners[i:]:
+            best = max(best, get_area(a, b))
+    print(f"Part 1: {best}")
+
+
+if __name__ == "__main__":
+    main()
